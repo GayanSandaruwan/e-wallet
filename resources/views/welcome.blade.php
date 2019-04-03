ab525688ded24d9fe19851678449176014e77fd0
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2019-04-03 07:10:34 +0530

    footer in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -141,5 +141,14 @@
         <script type="text/javascript" src="{{asset('mdb/js/mdb.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('mdb/js/addons/datatables.js')}}"></script>
         <script type="text/javascript" src="{{asset('mdb/js/addons/bootstrap-datepicker.js')}}"></script>
+        <footer class="page-footer font-small aqua-gradient">
+
+            <!-- Copyright -->
+            <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                <a href="https://mdbootstrap.com/education/bootstrap/"> ewalletlk.herokuapp.com</a>
+            </div>
+            <!-- Copyright -->
+
+        </footer>
     </body>
 </html>
